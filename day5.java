dabfad164019d31d1bd065ed483c7bb729f48505
Aniket Dobe <75830274+AniketDobe@users.noreply.github.com>
2023-12-26 21:03:55 +0530

    Update day5.java

--- day5.java
+++ day5.java
@@ -1,3 +1,6 @@
+//Given a string s, reverse only all the vowels in the string and return it.
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once
+
 class Solution {
     public String reverseVowels(String s) {
         char[] chars = s.toCharArray();
